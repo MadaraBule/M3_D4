f18f57b2fed0865556e477848294854b4040188e
MadaraBule <100915562+MadaraBule@users.noreply.github.com>
2022-04-07 15:29:46 +0300

    i got cards

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-fetch("https://striveschool-api.herokuapp.com/books")
+fetch("https://striveschool-api.herokuapp.com/books", { method: "GET" })
     .then(response => response.json())
     .then(books => {
         console.log(books);
+
+        const row = document.querySelector(".row")
+
+        row.innerHTML = books
+            .map(books => `
+                                    <div class="col-sm-4">
+                                        <div class="card">
+                                                <img src=${books.img} class="card-img-top" alt="...">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">${books.title}</h5>
+                                                    <p class="card-text">€ ${books.price}</p>
+                                                    <p class="card-text">${books.category}</a>
+                                                    <button type="button" class="btn btn-light">Add to Cart</button>
+                                                    <button type="button" class="btn btn-light">Skip</button>
+                                                </div>
+                                        </div>
+                                    </div>`)
     })
+    .catch(err => console.log(err))
